7da1acdd850438f4fec4040b4b1456ab1b350fd2
Thafer Shahin <thafershahin@asads-macbook-air.local>
2018-08-21 01:11:40 +0400

    Initial Commit

---
+++ SearchMovies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchMovies
+//
+//  Created by Thafer Shahin on 8/21/18.
+//  Copyright © 2018 Thafer Shahin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
